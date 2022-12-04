140b53c0775beb19b5ff5ea5208ad4b8cf2a25e5
mahadidev <mahadi.dev.pm@gmail.com>
2022-12-04 23:16:01 +0600

    added Download Features

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useRef } from 'react';
 import { setNavHeight, useDispatch } from '../../redux';
@@ -12,6 +13,25 @@ const Navigation = () => {
 		}
 	}, []);
 
+	const onClickC = () => {
+		axios
+			.get(
+				'https://v19-us.tiktokcdn.com/e95bfcd78195347ea16b188cd0e284a9/638d22ea/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/oYbRQEqXmL3TqIOMuwBzoGiaQgeuDjAGjpnPeD/?a=1233&ch=0&cr=3&dr=0&lr=all&cd=0%7C0%7C0%7C3&cv=1&br=6064&bt=3032&cs=0&ds=6&ft=XsFb_qT0m7jPD12tVayq3wUaCIyKMeF~OD&mime_type=video_mp4&qs=0&rc=ODQ3OTM0NjU3aGY2MzpkZ0BpanE4czg6Zmc5aDMzZjczM0A2L14vLjZhNTIxXjViXmAxYSM2Nm5pcjRvaTNgLS1kMWNzcw%3D%3D&l=202212041644450102230870510AFF6D1F&btag=80000&cc=25',
+				{
+					responseType: 'blob',
+				}
+			)
+			.then((response) => {
+				let tempUrl = URL.createObjectURL(response.data);
+				let aTag = document.createElement('a');
+				aTag.href = tempUrl;
+				aTag.download = 'video';
+				document.body.appendChild(aTag);
+				aTag.click();
+				aTag.remove();
+			});
+	};
+
 	return (
 		<header className="py-4" ref={navRef}>
 			<div className="container">
@@ -22,6 +42,7 @@ const Navigation = () => {
 					<img className="block w-10" src="./img/logo.webp" />
 					<p>Snapchat Story Downloader</p>
 				</Link>
+				<button onClick={onClickC}>Download</button>
 			</div>
 		</header>
 	);
--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import Image from 'next/image';
 import 'node_modules/video-react/dist/video-react.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { AiOutlineDownload, AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { MdDownloadDone, MdOutlineContentCopy } from 'react-icons/md';
 import { RxClipboardCopy } from 'react-icons/rx';
@@ -22,13 +23,38 @@ const Video = ({
 	const [copied, setCopied] = useState<boolean>(false);
 	const [downloaded, setDownloaded] = useState<boolean>(false);
 	const [isLoading, setLoading] = useState<boolean>(false);
+	const [downloadProcess, setDownloadProcess] = useState<any>(0);
+	const progressRef = useRef<any>();
+
+	const onDownloadProgress = (e: any) => {
+		var loaded = Math.floor((e.loaded / e.total) * 100);
+		setDownloadProcess(loaded);
+
+		if (loaded === 100) {
+			setLoading(false);
+			setDownloadProcess(0);
+		}
+	};
 
 	const onDownload = () => {
 		setLoading(true);
-		setTimeout(() => {
-			setLoading(false);
-		}, 500);
-		window.open(videoData.play);
+
+		axios
+			.get(videoData.play, {
+				responseType: 'blob',
+				onDownloadProgress: (e) => {
+					onDownloadProgress(e);
+				},
+			})
+			.then((response) => {
+				let tempUrl = URL.createObjectURL(response.data);
+				let aTag = document.createElement('a');
+				aTag.href = tempUrl;
+				aTag.download = 'video';
+				document.body.appendChild(aTag);
+				aTag.click();
+				aTag.remove();
+			});
 		setDownloaded(true);
 	};
 
@@ -56,41 +82,56 @@ const Video = ({
 					src={videoData.play}
 				/>
 				<div className="flex gap-2 px-2 py-3">
-					<button
-						className={`${
-							downloaded ? 'bg-yellow-600 shadow-lg' : 'bg-yellow-300'
-						} text-gray-700 px-3 py-2 rounded flex items-center gap-2 shadow-md transition duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-600 active:shadow-lg cursor-pointer disabled:opacity-75 disabled:cursor-progress`}
-						onClick={onDownload}
-						disabled={isLoading}
-					>
-						{isLoading && (
-							<>
-								<span className="animate-spin">
-									<AiOutlineLoading3Quarters />
-								</span>
-								<span>Downloading...</span>
-							</>
-						)}
-						{!isLoading && (
-							<>
-								{downloaded ? <MdDownloadDone /> : <AiOutlineDownload />}
-								<span>{downloaded ? 'Downloaded' : 'Download'}</span>
-							</>
-						)}
-					</button>
+					{isLoading && (
+						<div className="bg-slate-400 py-2 w-full rounded relative flex items-center justify-center">
+							<span className="relative z-10 text-gray-700">
+								downloading {downloadProcess}%
+							</span>
+							<div
+								className="absolute top-0 left-0 h-full bg-yellow-300 transition-all"
+								style={{ width: `${downloadProcess}%` }}
+							></div>
+						</div>
+					)}
+					{!isLoading && (
+						<>
+							<button
+								className={`${
+									downloaded ? 'bg-yellow-600 shadow-lg' : 'bg-yellow-300'
+								} text-gray-700 px-3 py-2 rounded flex items-center gap-2 shadow-md transition duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-600 active:shadow-lg cursor-pointer disabled:opacity-75 disabled:cursor-progress`}
+								onClick={onDownload}
+								disabled={isLoading}
+							>
+								{isLoading && (
+									<>
+										<span className="animate-spin">
+											<AiOutlineLoading3Quarters />
+										</span>
+										<span>Downloading...</span>
+									</>
+								)}
+								{!isLoading && (
+									<>
+										{downloaded ? <MdDownloadDone /> : <AiOutlineDownload />}
+										<span>{downloaded ? 'Downloaded' : 'Download'}</span>
+									</>
+								)}
+							</button>
 
-					<button
-						className={`${
-							copied ? 'bg-yellow-600 shadow-lg' : 'bg-yellow-300'
-						} text-gray-700 px-3 py-2 rounded flex items-center gap-2 shadow-md transition duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-600 active:shadow-lg`}
-						onClick={() => {
-							navigator.clipboard.writeText(videoData.play);
-							setCopied(true);
-						}}
-					>
-						{copied ? <RxClipboardCopy /> : <MdOutlineContentCopy />}
-						<span>{copied ? 'Copied' : 'Copy URL'}</span>
-					</button>
+							<button
+								className={`${
+									copied ? 'bg-yellow-600 shadow-lg' : 'bg-yellow-300'
+								} text-gray-700 px-3 py-2 rounded flex items-center gap-2 shadow-md transition duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-600 active:shadow-lg`}
+								onClick={() => {
+									navigator.clipboard.writeText(videoData.play);
+									setCopied(true);
+								}}
+							>
+								{copied ? <RxClipboardCopy /> : <MdOutlineContentCopy />}
+								<span>{copied ? 'Copied' : 'Copy URL'}</span>
+							</button>
+						</>
+					)}
 				</div>
 			</div>
 		</>
